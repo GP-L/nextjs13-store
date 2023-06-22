0dcecb2a7166ae784b606ece55431ef8ff15f795
Gianluca Pierre-Louis <gianlucaspl@gmail.com>
2023-06-22 14:41:25 -0500

    Load the stripe products on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
+import Stripe from "stripe";
+
+const getStripeProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+    apiVersion: "2022-11-15",
+  });
+  const res = await stripe.prices.list({
+    expand: ["data.product"],
+  });
+  const prices = res.data;
+  return prices;
+};
+
+export default async function Home() {
+  const products = await getStripeProducts();
+  console.log(products);
   return <main className="p-4"></main>;
 }
